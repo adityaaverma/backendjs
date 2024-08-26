c80185e6d0034b519d6eb9d823a9800b3ae651cc
adityaaverma <aditya.aryan.ug20@nsut.ac.in>
2024-08-26 06:45:40 +0100

    connected to mongoAtlas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv'
-import mongoos from 'mongoose'
-import { DB_NAME } from './constants'
+
 import express from 'express'
-import connectDB from "./db"
+import connectDB from "./db/index.js"
+
+
 dotenv.config({
     path:'./env'
 })
+connectDB();

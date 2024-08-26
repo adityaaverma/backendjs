efd0ee0aee0c5492bb4db1a2e994cbbdbd271d4a
adityaaverma <aditya.aryan.ug20@nsut.ac.in>
2024-08-26 08:58:16 +0100

    updated utils

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-import express from 'express'
-import connectDB from "./db/index.js"
-
+import express from "express";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
-connectDB();
+  path: "./env",
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server started at http://localhost:${process.env.PORT}`);
+      console.log(`DB Name: ${process.env.DB_NAME}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!!", err);
+  });
